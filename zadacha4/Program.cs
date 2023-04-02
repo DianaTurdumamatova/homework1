8976080b3040463bd3801f49f04ca2aa14381038
Diana <danon170801@gmail.com>
2023-04-02 19:45:41 +0600

    решили задачу 4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает три числа и 
+// выдает максимаальное из этих чисел.
+// Например: 
+// 2,3,7 -> 7
+// 44,5,9 -> 44
+
+Console.Clear();
+Console.Write("Введите первое число:");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число:");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число:");
+int num3 = int.Parse(Console.ReadLine());
+int max = 0;
+if(num1 > max);
+{
+    max = num1; 
+}if(num2 > max)
+{
+    max = num2;
+}if(num3 > max)
+{
+    max = num3;
+}
+Console.WriteLine("max = " + max);
